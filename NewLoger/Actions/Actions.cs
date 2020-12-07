80080ac88648e8f6d4d7ad9f79308963cbceb897
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-08 00:28:40 +0200

    Added Actions class

---
+++ NewLoger/Actions/Actions.cs
@@ -0,0 +1,49 @@
+﻿namespace NewLoger.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using NewLoger.Enum;
+    using NewLoger.Helper;
+    using NewLoger.Logger;
+
+    /// <summary>
+    /// Class when we will throw exeptions and info messages.
+    /// </summary>
+    public class Actions
+    {
+        private readonly Logger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Actions"/> class.
+        /// </summary>
+        public Actions()
+        {
+            this.logger = Logger.GetInstance();
+        }
+
+        /// <summary>
+        /// Method with throw info message.
+        /// </summary>
+        public void InfoMessage()
+        {
+            this.logger.Log(LogType.Info, $"Info message{nameof(this.InfoMessage)}");
+        }
+
+        /// <summary>
+        /// Method with throw exeption message.
+        /// </summary>
+        public void ExeptionMessage()
+        {
+            throw new Exception("I broke a toilet(((");
+        }
+
+        /// <summary>
+        /// Method with throw Business exeption message.
+        /// </summary>
+        public void BusinessExeptionMessage()
+        {
+            throw new BusinessExeption($"{nameof(this.BusinessExeptionMessage)}");
+        }
+    }
+}
