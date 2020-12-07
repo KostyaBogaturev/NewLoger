18c09dc261dd6c53d3b8e6999aeb2125a690ca6a
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-08 00:04:27 +0200

    Created business exeption

---
+++ NewLoger/Helper/BusinessExeption.cs
@@ -0,0 +1,21 @@
+﻿namespace NewLoger.Helper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// New custom exeption.
+    /// </summary>
+    public class BusinessExeption : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BusinessExeption"/> class.
+        /// </summary>
+        /// <param name="methodName">text of smessage.</param>
+        public BusinessExeption(string methodName)
+        : base($"Skipped logic in method:{methodName}")
+        {
+        }
+    }
+}
